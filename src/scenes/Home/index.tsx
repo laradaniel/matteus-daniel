82f5959e2db4f8f2d51b3aeb7c2a019feb930efb
Lara Daniel <88368957+laradaniel@users.noreply.github.com>
2024-09-12 21:13:15 -0300

    feat: created Home

---
+++ src/scenes/Home/index.tsx
@@ -0,0 +1,113 @@
+import React, { FC, useEffect, useState } from 'react'
+import {
+  Banner,
+  Investment,
+  Footer,
+  Whatsapp,
+  Feedbacks,
+} from '../../components'
+import {
+  MenuContainerDesktop,
+  MenuContainerMobile,
+  Button,
+  ButtonsDesktopWrapper,
+  IconMenuMobileClosed,
+  BoxBurgerMenu,
+  MenuMobileOptions,
+  IconMenuMobileOpen,
+  ButtonMobile,
+  BoxCloseMenu,
+  BoxIcons,
+} from './styles'
+import { useParams } from 'react-router-dom'
+import { Icone } from '../../assets/images'
+import { Box } from '@mui/material'
+
+const Home: FC = () => {
+  const { module } = useParams()
+
+  const [openMenu, setOpenMenu] = useState(false)
+
+  const handleToggle = () => {
+    setOpenMenu(!openMenu)
+  }
+
+  const navigateTo = (module: string) => {
+    const container = document.getElementById(module)
+    container?.scrollIntoView({ behavior: 'smooth' })
+    setOpenMenu(!openMenu)
+  }
+
+  useEffect(() => {
+    if (module) {
+      const container = document.getElementById(module)
+      container?.scrollIntoView({ behavior: 'smooth' })
+    }
+  }, [module])
+
+  return (
+    <>
+      <MenuContainerDesktop>
+        <Box marginLeft="10px" alignItems="center">
+          <img src={Icone} width="17%" />
+        </Box>
+        <ButtonsDesktopWrapper>
+          <Button onClick={() => navigateTo('banner')}>Início</Button>
+          <Button onClick={() => navigateTo('about')}>Sobre nós</Button>
+          <Button onClick={() => navigateTo('projects')}>Projetos</Button>
+          <Button onClick={() => navigateTo('partners')}>Parceiros</Button>
+          <Button onClick={() => navigateTo('')}>Peça seu orçamento</Button>
+          <Button onClick={() => navigateTo('contacts')}>Contato</Button>
+        </ButtonsDesktopWrapper>
+      </MenuContainerDesktop>
+
+      <MenuContainerMobile>
+        <BoxIcons>
+          <BoxBurgerMenu open={openMenu} onClick={handleToggle}>
+            <IconMenuMobileClosed />
+          </BoxBurgerMenu>
+          <BoxCloseMenu open={openMenu} onClick={handleToggle}>
+            <IconMenuMobileOpen />
+          </BoxCloseMenu>
+        </BoxIcons>
+        <MenuMobileOptions open={openMenu}>
+          <ButtonMobile onClick={() => navigateTo('banner')}>
+            Início
+          </ButtonMobile>
+          <ButtonMobile onClick={() => navigateTo('about')}>
+            Sobre nós
+          </ButtonMobile>
+          <ButtonMobile onClick={() => navigateTo('projects')}>
+            Projetos
+          </ButtonMobile>
+          <ButtonMobile onClick={() => navigateTo('partners')}>
+            Parceiros
+          </ButtonMobile>
+          <ButtonMobile onClick={() => navigateTo('packages')}>
+            Peça seu orçamento
+          </ButtonMobile>
+          <ButtonMobile onClick={() => navigateTo('contacts')}>
+            Contato
+          </ButtonMobile>
+        </MenuMobileOptions>
+      </MenuContainerMobile>
+
+      <div id="banner">
+        <Banner />
+      </div>
+      <div id="projects">
+        <Feedbacks />
+      </div>
+      <div id="investments">
+        <Investment />
+      </div>
+      <div id="contacts">
+        <Footer />
+      </div>
+
+      <Whatsapp />
+    </>
+  )
+}
+
+export default Home
---
+++ src/scenes/Home/styles.ts
@@ -0,0 +1,165 @@
+import styled, { css } from 'styled-components'
+import { Box, Button as _Button } from '@mui/material'
+import { getColorTheme } from '../../theme/colors'
+import MenuIcon from '@mui/icons-material/Menu'
+import CloseIcon from '@mui/icons-material/Close'
+
+type MenuProps = {
+  open: boolean
+}
+
+const secondaryColor = getColorTheme('secondary')
+const lightColor = getColorTheme('light')
+
+export const BoxMenu = styled(Box)``
+
+export const MenuContainerDesktop = styled(Box)`
+  width: 100%;
+  height: 50px;
+  background-color: ${lightColor};
+  display: flex;
+  align-items: center;
+  justify-content: end;
+
+  @media (max-width: 970px) {
+    display: none;
+  }
+`
+
+export const MenuContainerMobile = styled(Box)`
+  display: none;
+  @media (max-width: 970px) {
+    display: flex;
+    background-color: ${secondaryColor};
+    width: 100%;
+    justify-content: center;
+    align-items: flex-end;
+    position: fixed;
+    z-index: 10;
+    flex-direction: column;
+  }
+`
+
+export const MenuMobileOptions = styled(Box)<MenuProps>`
+  display: none;
+
+  @media (max-width: 970px) {
+    width: 100%;
+    height: 200px;
+    background-color: ${secondaryColor};
+    align-items: flex-end;
+    flex-direction: column;
+
+    ${({ open }) =>
+      open &&
+      css`
+        display: flex;
+      `}
+  }
+`
+
+export const ButtonsDesktopWrapper = styled(Box)`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: end;
+
+  @media (max-width: 970px) {
+    display: none;
+  }
+`
+
+export const IconMenuMobileClosed = styled(MenuIcon)`
+  color: ${lightColor};
+  justify-content: flex-end;
+  margin-right: 10px;
+
+  &.css-i4bv87-MuiSvgIcon-root {
+    font-size: 30px;
+  }
+`
+
+export const IconMenuMobileOpen = styled(CloseIcon)`
+  color: ${lightColor};
+  justify-content: flex-end;
+  margin-right: 10px;
+  &.css-i4bv87-MuiSvgIcon-root {
+    font-size: 32px;
+  }
+`
+
+export const BoxBurgerMenu = styled(Box)<MenuProps>`
+  display: flex;
+
+  ${({ open }) =>
+    open &&
+    css`
+      display: none;
+    `}
+`
+
+export const BoxCloseMenu = styled(Box)<MenuProps>`
+  display: none;
+
+  ${({ open }) =>
+    open &&
+    css`
+      display: flex;
+    `}
+`
+
+export const BoxIcons = styled(Box)`
+  display: flex;
+  height: 45px;
+  align-items: center;
+`
+
+export const ButtonsMobileWrapper = styled(Box)`
+  width: 100%;
+  align-items: flex-end;
+  flex-direction: column;
+`
+
+export const Button = styled(_Button).attrs({
+  variant: 'contained',
+})`
+  &.MuiButton-contained {
+    margin-right: 30px;
+    color: ${secondaryColor};
+    background-color: ${lightColor};
+    border: 1px solid ${lightColor};
+    padding: 0 10px 0 10px;
+    text-transform: none;
+    border-radius: 32px;
+    box-shadow: 0px 0px 0px 0px;
+    font-weight: bold;
+  }
+
+  &.MuiButton-contained:hover {
+    margin-right: 30px;
+    color: ${secondaryColor};
+    background-color: ${lightColor};
+    border: 1px solid ${secondaryColor};
+    padding: 0 10px 0 10px;
+    text-transform: none;
+    border-radius: 32px;
+    box-shadow: 0px 0px 0px 0px;
+    font-weight: bold;
+  }
+`
+
+export const ButtonMobile = styled(_Button)`
+  &.MuiButton-root {
+    color: ${lightColor};
+    text-transform: none;
+    box-shadow: 0px 0px 0px 0px;
+    line-height: 12px;
+    font-size: 11px;
+    justify-content: end;
+    margin-right: 5px;
+  }
+
+  &.MuiButton-root:hover {
+    text-decoration: underline;
+  }
+`
